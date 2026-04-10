24993d6a7d83d626eb72b08d54a284dc9aff28ae
Danil6789 <sotnikov6789@gmail.com>
2026-04-10 14:49:16 +0400

    feat(user): add user entity and user repository

---
+++ src/main/java/com/example/cloud_storage/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.cloud_storage.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 60)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/example/cloud_storage/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.cloud_storage.repository;
+
+import com.example.cloud_storage.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

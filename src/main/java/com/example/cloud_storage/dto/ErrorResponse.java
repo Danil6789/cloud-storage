3be1ae7594e0581c542eb4bbfc31d731e7bd0290
Danil6789 <sotnikov6789@gmail.com>
2026-04-11 19:58:46 +0400

    feat(common): add global exception handler and add error response dto

---
+++ src/main/java/com/example/cloud_storage/dto/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.example.cloud_storage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
---
+++ src/main/java/com/example/cloud_storage/handler/GlobalHandlerException.java
@@ -0,0 +1,23 @@
+package com.example.cloud_storage.handler;
+
+import com.example.cloud_storage.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalHandlerException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+
+        return new ErrorResponse(message);
+    }
+}
